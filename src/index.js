615f606258f63801c492aea60fe6ac8b4c061125
Artsiom Drushchyts <temafm-3@yandex.ru>
2019-02-23 15:04:41 +0300

    love-triangle-completed

--- src/index.js
+++ src/index.js
@@ -4,4 +4,15 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let count = 0;
+  let lover1, 
+      lover2;
+  for(let i = 0; i<preferences.length; i++) {
+    lover1 = preferences[i] - 1;
+    lover2 = preferences[lover1] - 1;
+    if(preferences[i] != preferences[lover1] && preferences[i] != preferences[lover2] && preferences[lover1] != preferences[lover2] && preferences[lover2] == i+1) {
+      count++;
+    }
+  }
+  return count/3;
 };
